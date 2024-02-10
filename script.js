0f7a02b9bc350ad14add608a2ea52c19c60ebed1
kannanATexcelforte <kannan@excelforte.com>
2024-02-10 15:18:05 +0530

    10.02.2024-cdtij
    completed partially

--- script.js
+++ script.js
@@ -1,81 +1,46 @@
-// Declare two variables, num1 and num2, and
-// assign them numeric values. Add these two variables and
-// store the result in a third variable named sum.
+// Create a function addNumbers that takes two arguments, a (a string
+// representing a number) and b (a number). Convert a to a number, add it to b, and
+// return the result.
 
-let num1 = 5;
-let num2 = 10;
-const sum = num1 + num2;
-console.log(sum);
-
-// Create a variable greeting and assign it a string
-// value. Then create another variable name and assign it a
-// string. Concatenate these two variables and print the
-// result.
-
-let greeting = 'Hello';
-let name = 'Kannan';
-console.log(greeting  + ' ' + name);
-
-// Declare a variable isValid and assign it a boolean
-// value. Check if isValid is true, and if so, print "Valid".
-// Otherwise, print "Invalid".
-
-let isValid = true;
-if(isValid) {
-    console.log('Valid');
-} else {
-    console.log('Invalid');
+function addNumbers(a, b) {
+    // let num = Number(a);
+    let num = parseInt(a);
+    return num + b;
 }
 
-// Write a JavaScript program to swap the values
-// of two variables a and b.
+const result = addNumbers('42', 8);
+console.log(result);
 
-let a = 10;
-let b = 20;
-[a, b] = [b, a];
-console.log(a, b);
+// Create a function concatenateStrings that takes two arguments, a (a
+// number) and b (a string). Convert a to a string, concatenate it with b, and return
+// the result.
 
-// Create a variable undefinedVar without
-// assigning any value to it. Print its value and data type.
+function concatenateStrings(a, b) {
+    let string = String(a);
+    let result = string + b;
+    return result;
+}
 
-let undefinedVar;
-console.log(undefinedVar);
-console.log(typeof undefinedVar);
+console.log(concatenateStrings(42, ' is the answer'));
 
-// Declare a constant PI and assign it the value of
-// 3.14. Attempt to reassign it to a different value and
-// observe the result.
+// Create a function checkFalsy that takes a value as an argument.
+// Convert the value to a boolean and return true if the converted value is falsy;
+// otherwise, return false.
 
-const PI = 3.14;
-// PI = 314;
-// console.log(PI); // make it error
+function checkFalsy(value) {
+    let bool = Boolean(value);
+    return !bool;
+}
 
-// Create a variable nullVar and explicitly set its
-// value to null. Print its type.
+console.log(checkFalsy(''));
 
-let nullVar = null;
-console.log(typeof nullVar);
+// Create a function multiplyByTwo that takes a string str containing an
+// integer. Parse the integer from str, multiply it by 2, and return the result as a
+// number.
 
-// Convert the string "123" into a number without
-// using any built-in JavaScript
+function multiplyByTwo(str) {
+    let strNum = parseInt(str) * 2;
+    return strNum;
+}
 
-let string = '123';
-let num = +string;
-console.log(typeof num);
-
-// Create an array with four different data types
-// (string, number, boolean, and null), and print the array
-// and its length.
-
-let arr = ['hi', 123, true, null];
-console.log(arr.length);
-
-// Declare a variable count with a number value.
-// Use the typeof operator to check its type, change the
-// value to a string, and check its type again.
-
-let count = 10;
-console.log(typeof count);
-count = count.toString();
-console.log(count);
-console.log(typeof count);
+console.log(multiplyByTwo(50));
