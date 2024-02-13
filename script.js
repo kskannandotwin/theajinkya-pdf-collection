3b7df8bd996148c14da72d252fed35bff52dc6e9
kannanATexcelforte <kannan@excelforte.com>
2024-02-13 14:53:32 +0530

    merged new

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Create a string that includes both single and double quotes using escape
 // characters.
 
-let mixedQuotes = "I'm learning Javascript and it's \"awesome\"!";
+let mixedQuotes = "I'm learning Javascript and it's \"awesome\"!";// Create a string that includes both single and double quotes using escape
+// characters.
+
