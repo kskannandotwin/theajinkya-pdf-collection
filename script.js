d063f80e5972a86f8ffd95df7972a4406f09d4c1
kannanATexcelforte <kannan@excelforte.com>
2024-02-15 16:31:27 +0530

    15.02.2024-functions
    going on

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-// Create a string that includes both single and double quotes using escape
-// characters.
+// Write a function named add that takes two arguments and returns their sum.
 
-let mixedQuotes = "I'm learning Javascript and it's \"awesome\"!";
-console.log(mixedQuotes);
+function add(a, b) {
+    return a + b;
+}
 
-// Create a multiline string with newline and tab characters to format a list of items.
+const sum = add(10, 15);
+console.log(sum);
 
-let groceryList = "Groceries: \n\t- Apples\n\t- Bananas\n\t- Milk";
-console.log(groceryList);
+// How does a function expression differ from a function declaration?
 
-// Declare a string that contains a file path using backslashes. Ensure that the
-// backslashes are properly escaped.
+// function declaration
+function hello() {
+    console.log("Hello");
+}
 
-let filePath = "C:\\Users\\John\\Documents\\file.txt";
-console.log(filePath);
+// function expression
+const helloWorld = function() {
+    console.log('Hello World');
+};
 
-// Use escape sequences to create a string containing the Greek letter Omega (Ω)
-// using its Unicode code point.
+// What is an Immediately Invoked Function Expression (IIFE) and provide an example?
 
-let omega = "\u03A9";
-console.log(`Omega ${omega}`);
+(function() {
+    console.log('Hello, this is IIFE');
+})();
 
-// Create a string with a form feed character to simulate a page break in text.
+// Write a function square that returns the square of a number and use it to calculate the square of 4.
 
-let formattedText = "Section 1\n\fSection 2";
-console.log(formattedText);
+function square(num) {
+    return num * num;
+}
 
-// Declare a string that contains a single backslash using an escape character.
+console.log(square(4));
 
-let singleBackSlash = "This is a single backslash \\";
-console.log(singleBackSlash);
+// How can you create a function in JavaScript that remembers the state of variables from its last execution?
+// What is closure?
 
-// Create a string that combines multiple escape characters, including newline, tab,
-// and backslash.
+function counter() {
+    let count = 0;
+    return function() {
+        count += 1;
+        return count;
+    }
+}
+// above the closure example
 
-let complexString = "Line 1\n\tLine 2\\\n\t\tLine 3";
-console.log(complexString);
+const myCounter = counter();
+console.log(myCounter());
+console.log(myCounter());
 
-// Declare a variable with a name that includes a Unicode character (e.g., a Greek
-// letter) using a Unicode escape sequence.
+// What are default parameters in a function? Give an example.
 
-let variableWithUnicode = "Variable with Omega: \u03A9";
-console.log(variableWithUnicode);
+function name(name = 'Guest') {
+    return 'Hello ' + name;
+}
+
+console.log(name('Kannan'));
+console.log(name());
