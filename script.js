13f7b211849e437aba749359c93a976128e20801
kannanATexcelforte <kannan@excelforte.com>
2024-02-15 15:14:20 +0530

    15.02.2024-jog
    going on

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Write a program that calculates the area of a rectangle using the
 // formula area = length × width. Prompt the user for the length and width of the
 // rectangle.
+
+let length = parseFloat(prompt("Enter the length of the rectangle:"));
+let width = parseFloat(prompt("Enter the width of the rectangle:"));
+
+let area = length * width;
