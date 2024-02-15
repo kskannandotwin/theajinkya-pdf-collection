f39de95557eb1b1ba9f3d07f3c1ac6fa4cf554de
kskannandotwin <kskannan.win@gmail.com>
2024-02-15 15:20:20 +0530

    15.02.2024-jog
    going on

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ let length = parseFloat(prompt("Enter the length of the rectangle:"));
 let width = parseFloat(prompt("Enter the width of the rectangle:"));
 
 let area = length * width;
+console.log(area);
